3fb49450b4b197e94e3935e7911ccee64ed3f627
Andy Thomson <andrew.thomson@4projects.com>
2014-12-17 15:04:06 +0000

    Initial Project Setup

---
+++ Xbim.BCF/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿﻿#region XbimHeader
+
+// The eXtensible Building Information Modelling (xBIM) Toolkit
+// Solution:    Xbim.BCF
+// Project:     Xbim.BCF
+// Filename:    AssemblyInfo.cs
+// (See accompanying copyright.rtf)
+
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xbim.BCF")]
+[assembly: AssemblyDescription("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9f41265-9ea7-4b3f-8e85-978f7cc9a9ee")]
